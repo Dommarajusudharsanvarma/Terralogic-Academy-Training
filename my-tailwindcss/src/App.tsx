cfaab8ed5bc859fba9169f69f656d9783673f4e9
sudharsan varma <dommarajusudharsanvarma@gmail.com>
2025-10-20 12:27:22 +0530

    latest code

---
+++ my-tailwindcss/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-blue-500">
+      {/* <header className="App-header">
+        
+      </header> */}
+    the text
+    </div>
+  );
+}
+
+export default App;
