c608d064b6f50aba7fe02aa2f06e58d5b678d548
sudharsan varma <dommarajusudharsanvarma@gmail.com>
2025-11-02 12:40:45 +0530

    latest code

---
+++ form-validation-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import FormWithErrors from './FormWithErrors';
+//import CustomForm from './CustomForm';
+//import MyForm from './MyForm';
+// import MyInput from './MyInput';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <MyInput label="Name:" />  */}
+      {/* <MyForm />
+      <CustomForm /> */}
+      <FormWithErrors />
+
+    </div>
+  );
+}
+
+export default App;
