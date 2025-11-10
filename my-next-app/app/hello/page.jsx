ae9c6a32c6ed7ecb83c4f517b02a2f023266f0f9
sudharsan varma <dommarajusudharsanvarma@gmail.com>
2025-11-10 15:39:49 +0530

    code

---
+++ my-next-app/app/hello/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GoogleMapComponent from './MyGoogleMap'
+import HomePage from './MyCustomPage'
+const page = () => {
+  return (
+    <div>
+        <GoogleMapComponent />
+        <HomePage/>
+    </div>
+
+  )
+}
+
+export default page
